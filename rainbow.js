ce803804c403ed93cb4247ced5fb3356e1a73fc2
javekk <periniraffaele@gmail.com>
2018-10-11 17:25:08 +0200

    add second step of the rainbow

--- rainbow.js
+++ rainbow.js
@@ -12,7 +12,7 @@ var areaHeight;
 var area = {
     draw : function(){
         area.clear();
-        RainbowWeever.drawStepByStep(context, 0, 0, 100, 200);
+        RainbowWeever.drawStepByStep(context, 0, 0, 100, 200, 300);
     } ,
     clear : function() {
         context.clearRect(-(areaWidth / 2), -(areaHeight / 2), canvas.width, canvas.height);
@@ -22,36 +22,78 @@ var area = {
 
 var RainbowWeever = {
 
-    totalIncrement : 100,
+    actualStep : 1,
 
     actualIncrement : 1,
 
-    drawStepByStep : function(context, centerX, centerY, internalRadius, externalRadius){
+    drawStepByStep : function(context, centerX, centerY, internalRadius, externalRadius, duration){
         
         //Circles center
         context.translate(centerX, centerY);
         
-        //Every Frame update 
+        //Every Frame update, reset for each step
         this.actualIncrement++;
 
-        var percentageIncrement = this.actualIncrement / this.totalIncrement;
-        
+        //Size of the rainbow
         var size = externalRadius - internalRadius;
+        
+        //Duration in frame
+        var totalFrames = duration;
+        
+        //Different frame numbers for each step the ratio io 2-6-2
+        var frameStep1 = 100;
+        var frameStep2 = 300;
+        var frameStep3 = 100;
 
+        
         //=====STEP 1=====
         //first step, drawing the first line
         //2 seconds => frames = fps*2
 
-        if(this.actualIncrement > this.totalIncrement){
-            this.actualIncrement = 1;
-        }
+        //if it is step 1 we use the percentaga otherwise the maxium
+        var step1Incr = this.actualStep == 1 ? this.actualIncrement / frameStep1 : 1;
 
         var tmpStartingPoint = -internalRadius - (size / 2);
         
         context.beginPath();
-        context.moveTo(tmpStartingPoint - ((size / 2) * percentageIncrement) , 0);
-        context.lineTo(tmpStartingPoint + (size * percentageIncrement), 0);
+        context.moveTo(tmpStartingPoint - ((size / 2) * step1Incr) , 0);
+        context.lineTo(tmpStartingPoint + ((size / 2) * step1Incr), 0);
         context.stroke();
+        
+
+        if(this.actualStep == 1 && this.actualIncrement > frameStep1-1){
+            this.actualStep = 2;
+            this.actualIncrement = 0;
+        }
+        
+        if(this.actualStep > 1){
+
+            //=====STEP 2=====
+            //Second step, drawing the arcs
+            //6 seconds => frames = fps*2
+
+            //if it is step 2 we use the percentaga otherwise the maxium
+            var step2Incr = this.actualStep == 2 ? this.actualIncrement / frameStep2 : 1;
+
+            var angle = Math.PI - (Math.PI * (step2Incr+0.00001));
+
+            for(var i = 0; i <= 7; i++){
+                var ratio = internalRadius + ((size / 7) * i);
+                context.beginPath();
+                context.arc(centerX, centerY, ratio, Math.PI, -angle, false);
+                context.stroke();
+            }
+
+            if(this.actualIncrement > frameStep2-1){
+                this.actualStep = 3;
+                this.actualIncrement = 0;
+            }
+        }
+
+        if(this.actualStep > 2){
+            
+        }
+
         
         //Go back  
         context.translate(-centerX, -centerY);
@@ -68,5 +110,4 @@ window.onload = function(){
     context.translate(areaWidth/2,areaHeight/2);
 
     this.interval = setInterval(area.draw, (1000/FPS));
-    
 }
